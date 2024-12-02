a584eabb56a664a0ed3a6559ae086c1de23f389a
DXRKN1GHT <komronbekzubaydullayev@gmail.com>
2024-12-02 19:46:31 +0500

    Added new feature

---
+++ courses/models.py
@@ -0,0 +1,9 @@
+# courses/models.py
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
